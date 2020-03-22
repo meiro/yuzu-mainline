d774ba29e04065e7920c84c8b057ff5f93b1c873
Roman Meier <romameie@student.ethz.ch>
2020-03-22 11:26:39 +0100

    Add test azure piplines config for flatpak release

---
+++ .ci/linux-flatpak/exec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+mkdir -p "$HOME/.ccache"
+
+# Configure docker and call the script that generates application data and build scripts
+docker run --env-file .travis/common/travis-ci.env --env-file .travis/linux-flatpak/travis-ci-flatpak.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache -v "$HOME/.ssh":/root/.ssh --privileged meirod/yuzu-test:latest /bin/bash -ex /yuzu/.travis/linux-flatpak/generate-data.sh
