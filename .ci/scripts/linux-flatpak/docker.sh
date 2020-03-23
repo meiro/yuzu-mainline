20a6a8f07c47061fd7ecfdc5c717693a92bb252f
Roman Meier <romameie@student.ethz.ch>
2020-03-23 13:25:53 +0100

    Revert "Try to reduce build time by telling flatpak to repo locally"
    This reverts commit f35379e5f235d29ef8ef77ad623dc6707c7affdd.

--- .ci/scripts/linux-flatpak/docker.sh
+++ .ci/scripts/linux-flatpak/docker.sh
@@ -4,8 +4,7 @@
 REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d'/' -f 2)
 CITRA_SRC_DIR="/yuzu"
 BUILD_DIR="$CITRA_SRC_DIR/build"
-REPO_DIR_REMOTE="$CITRA_SRC_DIR/repo"
-REPO_DIR="/tmp/repo"
+REPO_DIR="$CITRA_SRC_DIR/repo"
 STATE_DIR="$CITRA_SRC_DIR/.flatpak-builder"
 KEYS_ARCHIVE="/tmp/keys.tar"
 SSH_DIR="/upload"
@@ -30,16 +29,15 @@ gpg2 --import "$GPG_KEY"
 
 # Mount our flatpak repository
 mkdir -p "$REPO_DIR"
-mkdir -p "$REPO_DIR_REMOTE"
-sshfs "$FLATPAK_SSH_USER@$FLATPAK_SSH_HOSTNAME:$SSH_DIR_REMOTE" "$REPO_DIR_REMOTE" -C -p "$FLATPAK_SSH_PORT" -o IdentityFile="$SSH_KEY" -o "StrictHostKeyChecking=no" -o ServerAliveInterval=60
+sshfs "$FLATPAK_SSH_USER@$FLATPAK_SSH_HOSTNAME:$SSH_DIR" "$REPO_DIR" -C -p "$FLATPAK_SSH_PORT" -o IdentityFile="$SSH_KEY" -o "StrictHostKeyChecking=no" -o ServerAliveInterval=60
 
 # DEBUGGING STUFF:
-ls -la $REPO_DIR_REMOTE
-touch $REPO_DIR_REMOTE/TEST
-echo "TESTING" >> $REPO_DIR_REMOTE/TEST2
-echo "TESTING2" >> $REPO_DIR_REMOTE/TEST
-rm $REPO_DIR_REMOTE/TEST2
-rm $REPO_DIR_REMOTE/TEST
+ls -la $REPO_DIR
+touch $REPO_DIR/TEST
+echo "TESTING" >> $REPO_DIR/TEST2
+echo "TESTING2" >> $REPO_DIR/TEST
+rm $REPO_DIR/TEST2
+rm $REPO_DIR/TEST
 
 # setup ccache location
 mkdir -p "$STATE_DIR"
@@ -48,6 +46,3 @@ ln -sv --force /root/.ccache "$STATE_DIR/ccache"
 # Build the yuzu flatpak
 flatpak-builder -v --jobs=4 --ccache --force-clean --state-dir="$STATE_DIR" --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY" --repo="$REPO_DIR" "$BUILD_DIR" "/tmp/org.yuzu.$REPO_NAME.json"
 flatpak build-update-repo "$REPO_DIR" -v --generate-static-deltas --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY"
-
-cp -r $REPO_DIR/* $REPO_DIR/.* $REPO_DIR_REMOTE/
-
